5868faf67a553298dacfbac9b1851b531275b43c
henrywl618 <henrywlam+git@gmail.com>
2022-05-23 20:51:41 -0400

    completed exercise, added some styling

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,26 @@
-console.log("Let's get this party started!");
+//const axios = require('axios').default;
+const giphyAPIKey = 'ZaeawWciQX3HaEevh9Z9iSXjRXnCgsc1';
+
+async function getGifURL(tag,api_key){
+    const result = await axios.get('https://api.giphy.com/v1/gifs/random', {
+        params:{
+            api_key,
+            tag,},
+    });
+    const url =result.data.data.images.fixed_width.url;
+    return url
+}
+
+function appendImg(url){
+    $('#imgContainer').append($(`<img src=${url}>`)); //Append a newly created image with the given url to the image container div
+}
+
+$('#searchBtn').on('click', async ()=>{
+    const searchValue = $('#searchInput').val();
+    const gifURL = await getGifURL(searchValue, giphyAPIKey);
+    appendImg(gifURL);
+})
+
+$('#removeBtn').on('click', ()=>{
+    $('#imgContainer').empty();
+})
