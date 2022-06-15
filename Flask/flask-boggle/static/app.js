1b316283b5029f13e4041bf0ff2e8c7388c2d91c
Henry <henrywlam+github@gmail.com>
2022-06-15 00:36:04 -0400

    added boggle score

--- Flask/flask-boggle/static/app.js
+++ Flask/flask-boggle/static/app.js
@@ -1,6 +1,10 @@
 const $guessForm = $('#guessForm')
 const $guessInput = $('#guessInput')
 const $responseText = $('#response')
+const $scoreDisplay = $('#score')
+
+let score = 0
+const guesses = new Set()
 
 // submits http request to server with the form info
 async function submitGuess(event){
@@ -9,6 +13,10 @@ async function submitGuess(event){
 
     response = await getResponse(guess);
     updatePage(response);
+    updateScore(response,guess);
+
+    //empty the input field
+    $guessForm.trigger('reset');
 
 }
 
@@ -16,9 +24,19 @@ async function getResponse(word){
     return await axios.post('/',{guess:word});
 }
 
+
 function updatePage(response){
     $responseText.text(response.data.result);
 }
 
-//clickeven handler for the submit button
+//Update the score if the guess is not already been guessed and it is a word/on the board
+function updateScore(response,guess){
+    if(response.data.result === 'ok' && !guesses.has(guess)){
+        guesses.add(guess);
+        score += guess.length;
+        $scoreDisplay.text(score);
+    }
+}
+
+//clickevent handler for the submit button
 $guessForm.on('submit',submitGuess);
